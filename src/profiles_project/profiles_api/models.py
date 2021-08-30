feeeb19168e7f0d1e521855f108112b52562934e
Pham Khanh <khanhzodoi@gmail.com>
2021-08-30 15:40:43 +0700

    Modified UserProfileManger, UserProfile in profiles_api/models.py file

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -15,22 +15,22 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an email address.')
 
         email = self.normalize_email(email)
-        user = self.model(email=email,name=name)
+        user = self.model(email=email, name=name)
         
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name, password=None):
+    def create_superuser(self, email, name, password):
         """Creates and saves a new superuser with given details."""
 
         user = self.create_user(email, name, password)
 
-        user.is_suoperuser = True
+        user.is_superuser = True
         user.is_staff = True
 
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
@@ -47,7 +47,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects     = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Used to get a users full name."""
