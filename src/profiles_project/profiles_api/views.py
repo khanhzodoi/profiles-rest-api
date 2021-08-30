e8b64f16a8d75ea721ed19ddbb748e8bbf1f353a
Pham Khanh <khanhzodoi@gmail.com>
2021-08-30 17:08:21 +0700

    Created our first API view.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloAPIView(APIView):
+    """Test API View."""
+
+    def get(self, request, format=None):
+        """Returns a list of APIview features."""
+        an_apivew = [
+            'User HTTP methods as function (get, post, patch, put, delete)',
+            'It is similar to a tradiontional Django view',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs'
+        ]
+        
+        return Response({'message': 'hello', 'an_apiview': an_apivew})
--- src/profiles_project/profiles_project/urls.py
+++ src/profiles_project/profiles_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import include
+from django.urls import path,re_path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/', include('profiles_api.urls'))
 ]
