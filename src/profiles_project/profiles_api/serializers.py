1431ed3070f3492fe164abbd7da7a104185e19ae
Pham Khanh <khanhzodoi@gmail.com>
2021-09-06 19:58:38 +0700

    Added profile listing and creating feature.

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from . import models
 
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing our APIView."""
@@ -7,3 +7,25 @@ class HelloSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=10)
     email = serializers.EmailField(max_length=255, allow_blank=True)
 
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for our user profile objects."""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """Create and return a new user."""
+
+        user = models.UserProfile(
+            email=validated_data['email'],
+            name=validated_data['name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
