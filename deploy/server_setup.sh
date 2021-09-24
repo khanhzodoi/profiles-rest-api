6fc72fa3ddb301fd8481d0540e579b7ecf89c492
Pham Khanh <khanhzodoi@gmail.com>
2021-09-24 19:35:57 +0700

    Fixed python pip installation issue.

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -15,7 +15,7 @@ locale-gen en_GB.UTF-8
 echo "Installing dependencies..."
 apt-get update
 sudo add-apt-repository universe
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/profiles-rest-api
